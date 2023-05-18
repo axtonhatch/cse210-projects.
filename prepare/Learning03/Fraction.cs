c4478a2b269d1187a7763dd4505c800a5b8ecc1d
axtonhatch <axtonhatch2000@gmail.com>
2023-05-17 22:46:51 -0600

    yes

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -2,35 +2,35 @@ using System;
 
 public class Fraction
 {
-    private int _top;
-    private int _bottom;
+    private int top;
+    private int bottom;
 
     public Fraction()
     {
-        _top = 1;
-        _bottom = 1;
+        top = 1;
+        bottom = 1;
     }
 
     public Fraction(int wholeNum)
     {
-        _top = wholeNum;
-        _bottom = 1;
+        top = wholeNum;
+        bottom = 1;
     }
 
-    public Fraction(int top, int bottom)
+    public Fraction(int ttop, int bbottom)
     {
-        _top = top;
-        _bottom = bottom;
+        top = ttop;
+        bottom = bbottom;
     }
     public string GetFractionString()
     {
-        string text = $"{_top}/{_bottom}";
+        string text = $"{top}/{bottom}";
         return text;
     }
 
     public double GetDecimalValue()
     {
-        return (double)_top / (double)_bottom;
+        return (double)top / (double)bottom;
     }
 }
 
