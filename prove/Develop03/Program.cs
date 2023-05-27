5f5ba2cfb42b0f36f079f7434d8a0c02fd68a6f3
axtonhatch <axtonhatch2000@gmail.com>
2023-05-27 14:40:50 -0600

    edited the program and fixde errors

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,14 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to the scripture memorizer!");
+        Console.WriteLine("Welcome to the EPIC scripture memorizer!");
 
         Scripture myScripture = new Scripture("Genesis", "1", "26-27" , "And God said, Let us make man in our image, after our likeness: and let them have dominion over the fish of the sea, and over the fowl of the air, and over the cattle, and over all the earth, and over every creeping thing that creepeth upon the earth. So God created man in his own image, in the image of God created he him; male and female created he them.");
         bool finish = false;
         string answer;
         while (finish == false)
 
-        {Console.WriteLine ("Press the Enter key to continue or type quit to finish");
+        {Console.WriteLine ("Press the 'return' key to continue or type 'quit' to finish your study.");
         answer  = Console.ReadLine();
 
         myScripture.DisplayRenderedText();
